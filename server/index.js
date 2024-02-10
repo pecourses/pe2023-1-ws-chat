eb2ba99f02cc349655bc67b838208b9025d4d144
Viktoriia <pecourses>
2024-02-10 17:11:01 +0200

    Add socket server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,24 @@
 const http = require('http');
+const { Server } = require('socket.io');
 const app = require('./app');
 
 const PORT = process.env.PORT ?? 5000;
 
 const httpServer = http.createServer(app);
+
+const io = new Server(httpServer, { cors: { origin: '*' } });
+
+io.on('connection', socket => {
+  console.log('Connection established :>> ');
+  socket.emit('HELLO_SELF', 'Hello on service!');
+  socket.broadcast.emit('NEW_ANOTHER_SOCKET', 'New socket is added');
+  io.emit('TO_EVERYONE', 'Service will reboot at 5 minutes');
+
+  // підписатися на обробку
+  socket.on('TO_SERVER', payload => {
+    console.log('payload :>> ', payload);
+  });
+});
 
 httpServer.listen(PORT, () => {
   console.log(`Server is running!`);
