6769bd6bedff31d509bf8494d1c2a6867dc6d660
Viktoriia <pecourses>
2024-02-10 19:14:19 +0200

    Refactor socket_events to constants

--- client/src/api/ws.js
+++ client/src/api/ws.js
@@ -3,17 +3,22 @@ import {
   newMessageError,
   newMessageSuccess,
 } from '../store/slices/messagesSlice';
+import CONSTANTS from './../constants';
+
+const {
+  SOCKET_EVENTS: { NEW_MESSAGE, NEW_MESSAGE_SUCCESS, NEW_MESSAGE_ERROR },
+} = CONSTANTS;
 
 const socket = io('ws://localhost:5000');
 
-export const createMessage = payload => socket.emit('NEW_MESSAGE', payload);
+export const createMessage = payload => socket.emit(NEW_MESSAGE, payload);
 
 export const bringStoreToSocket = store => {
-  socket.on('NEW_MESSAGE_SUCCESS', payload => {
+  socket.on(NEW_MESSAGE_SUCCESS, payload => {
     store.dispatch(newMessageSuccess(payload));
   });
 
-  socket.on('NEW_MESSAGE_ERROR', error => {
+  socket.on(NEW_MESSAGE_ERROR, error => {
     store.dispatch(newMessageError(error));
   });
 };
--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ const http = require('http');
 const { Server } = require('socket.io');
 const app = require('./app');
 const { Message } = require('./models');
+const {
+  SOCKET_EVENTS: { NEW_MESSAGE, NEW_MESSAGE_SUCCESS, NEW_MESSAGE_ERROR },
+} = require('./constants');
 
 const PORT = process.env.PORT ?? 5000;
 
@@ -10,12 +13,12 @@ const httpServer = http.createServer(app);
 const io = new Server(httpServer, { cors: { origin: '*' } });
 
 io.on('connection', socket => {
-  socket.on('NEW_MESSAGE', async payload => {
+  socket.on(NEW_MESSAGE, async payload => {
     try {
       const createdMessage = await Message.create(payload);
-      io.emit('NEW_MESSAGE_SUCCESS', createdMessage);
+      io.emit(NEW_MESSAGE_SUCCESS, createdMessage);
     } catch (err) {
-      socket.emit('NEW_MESSAGE_ERROR', {
+      socket.emit(NEW_MESSAGE_ERROR, {
         error: err.message ?? 'Error',
       });
     }
