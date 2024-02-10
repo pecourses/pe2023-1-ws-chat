dd87aa1a228bf74cbc79ed67c0e6f841f6afc573
Viktoriia <pecourses>
2024-02-10 19:21:00 +0200

    Refactor server initSocket

--- server/index.js
+++ server/index.js
@@ -1,29 +1,13 @@
 const http = require('http');
-const { Server } = require('socket.io');
+
 const app = require('./app');
-const { Message } = require('./models');
-const {
-  SOCKET_EVENTS: { NEW_MESSAGE, NEW_MESSAGE_SUCCESS, NEW_MESSAGE_ERROR },
-} = require('./constants');
+const initSocket = require('./socket');
 
 const PORT = process.env.PORT ?? 5000;
 
 const httpServer = http.createServer(app);
 
-const io = new Server(httpServer, { cors: { origin: '*' } });
-
-io.on('connection', socket => {
-  socket.on(NEW_MESSAGE, async payload => {
-    try {
-      const createdMessage = await Message.create(payload);
-      io.emit(NEW_MESSAGE_SUCCESS, createdMessage);
-    } catch (err) {
-      socket.emit(NEW_MESSAGE_ERROR, {
-        error: err.message ?? 'Error',
-      });
-    }
-  });
-});
+initSocket(httpServer);
 
 httpServer.listen(PORT, () => {
   console.log(`Server is running!`);
